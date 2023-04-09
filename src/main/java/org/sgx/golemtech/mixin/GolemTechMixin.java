8cd24dd7e18788565fd4e89e5080aaff986d3b4b
petya <petya.podstavkin@mail.ru>
2023-04-10 01:50:36 +0700

    Ended renaming, empty mod works properly

--- src/main/java/org/sgx/golemtech/mixin/GolemTechMixin.java
+++ src/main/java/org/sgx/golemtech/mixin/GolemTechMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package org.sgx.golemtech.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import org.sgx.golemtech.GolemTechMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class GolemTechMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		GolemTechMod.LOGGER.info("This line is printed by an golemtech mod mixin!");
 	}
 }
